9ea14ed5388157cbb5d6d697330a5a18c32f64dc
Matt Chudoba <mchudoba5@gmail.com>
2015-04-03 20:32:42 -0400

    Added velocity to camera movem

--- Monopoly/Assets/__Scripts/CameraControl.cs
+++ Monopoly/Assets/__Scripts/CameraControl.cs
@@ -14,10 +14,15 @@ public class CameraControl : MonoBehaviour
 	public bool invertMoveY = false;
 	public float mapWidth = 40.0f;
 	public float mapHeight = 40.0f;
+	public float inertiaDuration = 1f;
+	public float minimumScrollVelocity = 100f;
 	
 	private Camera cam;
 	private float horizontalExtent, verticalExtent;
 	private float minX, maxX, minY, maxY;
+	private float scrollVelocity = 0f;
+	private float timeTouchPhaseEnded;
+	private Vector2 scrollDirection = Vector2.zero;
 	
 	void Start()
 	{
@@ -42,12 +47,29 @@ public class CameraControl : MonoBehaviour
 
 		Touch[] touches = Input.touches;
 
+		if (touches.Length < 1)
+		{
+			if (scrollVelocity != 0f)
+			{
+				float timer = (Time.time - timeTouchPhaseEnded) / inertiaDuration;
+				float frameVelocity = Mathf.Lerp(scrollVelocity, 0f, timer);
+				cam.transform.position += -(Vector3)scrollDirection.normalized * (frameVelocity * 0.005f) * Time.deltaTime;
+
+				if (timer >= 1f)
+					scrollVelocity = 0f;
+			}
+		}
+
 		if (touches.Length > 0)
 		{
 			// Single touch (move)
 			if (touches.Length == 1)
 			{
-				if (touches[0].phase == TouchPhase.Moved)
+				if (touches[0].phase == TouchPhase.Began)
+				{
+					scrollVelocity = 0f;
+				}
+				else if (touches[0].phase == TouchPhase.Moved)
 				{
 					Vector2 delta = touches[0].deltaPosition;
 
@@ -58,6 +80,16 @@ public class CameraControl : MonoBehaviour
 					positionY = invertMoveY ? positionY : positionY * -1f;
 
 					cam.transform.position += new Vector3(positionX, positionY, 0f);
+
+					scrollDirection = touches[0].deltaPosition.normalized;
+					scrollVelocity = touches[0].deltaPosition.magnitude / touches[0].deltaTime;
+
+					if (scrollVelocity <= minimumScrollVelocity)
+						scrollVelocity = 0f;
+				}
+				else if (touches[0].phase == TouchPhase.Ended)
+				{
+					timeTouchPhaseEnded = Time.time;
 				}
 			}
 
