5a8c0be2766fe7145b0ce7d6f741f1792a5395bd
Parthiv Vora <psvora@umich.edu>
2015-04-04 15:43:07 -0400

    Basic Dice

---
+++ Monopoly/Assets/__Scripts/Dice.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour {
+
+	public Material side1;
+	public Material side2;
+	public Material side3;
+	public Material side4;
+	public Material side5;
+	public Material side6;
+
+	Renderer r;
+
+	// Use this for initialization
+	void Start () {
+		r = gameObject.GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int side = Random.Range (1, 7);
+		switch (side) {
+		case 1:
+			r.material = side1;
+			break;
+		case 2:
+			r.material = side2;
+			break;
+		case 3:
+			r.material = side3;
+			break;
+		case 4:
+			r.material = side4;
+			break;
+		case 5:
+			r.material = side5;
+			break;
+		default:
+			r.material = side6;
+			break;
+		}
+	}
+}
